7f3ee275faa6d9785cd831f9f61706751d794a3a
itibd <rahulitibd@gmail.com>
2021-01-31 21:37:59 +0600

    inasital test

---
+++ test.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+
+var a string
+a="tomar name"
+var b string
+b="amar name"
+var c string
+c="rahul"
+fmt.Println(a,b,c)
+var t rune='M'
+var l int=30000000
+var n float64=365.2154
+fmt.Println(t,l,n)
+
+}
